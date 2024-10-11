5bd21b1da4ab23a3f74892482add9260d2b0aaec
Valentin-AC <valentinandreichindea@gmail.com>
2024-10-10 22:50:20 -0400

    Update HandRecog_CtrlSignals.py

--- HandRecog_CtrlSignals.py
+++ HandRecog_CtrlSignals.py
@@ -1,3 +1,17 @@
-print ("test")
-print ("test2")  
-#test
+import numpy as np
+import cv2
+#if you plan to run this code run the following commands into your command prompt
+#pip install numpy
+#pip install cv2
+#also, add directory for each to PATH (have to figure out how to do this myself)
+
+webcam_capture = cv2.VideoCapture(0)
+
+while True:
+    webcam_vid_stream = webcam_capture.read()
+
+    if cv2.waitKey(1) == ord('x'):
+        break
+
+webcam_capture.release()
+cv2.destroyAllWindows()
